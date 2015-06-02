930b27a37306224c7ad0441d710aa0d1fbbf925c
xichi <xichi@github.com>
2015-06-02 16:56:36 -0400

    OMG CARRIERWAVE

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+
+  mount_uploader :profile_picture, ProfilePictureUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601223218) do
+ActiveRecord::Schema.define(version: 20150602205319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20150601223218) do
     t.string   "facebook"
     t.string   "twitter"
     t.string   "linkedin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "profile_picture"
   end
 
 end

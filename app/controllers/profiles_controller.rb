2597b2fcb4becbdc65e8b4f6f6a09ca688988b53
xichi <xichi@github.com>
2015-06-04 10:31:58 -0400

    cleans code, adds authentication to several web pages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
- before_filter :authenticate_user!, except: [:index, :show]
+
+  skip_before_action :authenticate_user!, only: [:show]
+
   def show
     @user = User.find(params[:id])
   end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,5 +2,3 @@ class Enrollment < ActiveRecord::Base
   belongs_to :student, class_name: 'User'
   belongs_to :course
 end
-
- 

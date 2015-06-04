2daca33db177e22f8388811133a4091d99405d46
tmurray002@gmail.com <tmurray002@gmail.com>
2015-06-04 15:21:50 -0400

    updates enrollment controller

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,9 +1,10 @@
 class EnrollmentsController < ApplicationController
   def create
-    current_user.inspect
     @enrollment = Enrollment.new
+    @enrollment.course_id = params[:course_id]
+    @enrollment.student_id = current_user.id
     @enrollment.save
-  
+
 
     redirect_to "/profiles/#{current_user.id}"
   end

caacaef8d13279746c07e1a8db1b8044a745c110
xichi <xichi@github.com>
2015-06-05 12:14:53 -0400

    adds some testing to emrollment_test.rb

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,5 @@
 class Enrollment < ActiveRecord::Base
   belongs_to :student, class_name: 'User'
   belongs_to :course
+  validates_uniqueness_of :student, scope: [:course]
 end
--- test/models/enrollment_test.rb
+++ test/models/enrollment_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class EnrollmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "user cannot take the class they are teaching" do
+    tim = User.create!(email: "tmurray002@gmail.com", password: "12345678")
+    kenny = User.create!(email: "kenny@gmail.com", password: "12345678")
+    steven = User.create!(email: "steven@example.com", password: "12345678")
+    swag202 = Course.create!(topic: "master the art of swag")
+    swag202.teacher = tim
+    swag202.save
+
+    swag202.students << kenny
+    swag202.students << steven
+    swag202.students << tim
+
+    assert_equal 0, tim.courses_taken.size
+
+  end
+
+  test "user cannot take the same class twice" do
+    tim = User.create!(email: "tmurray002@gmail.com", password: "12345678")
+    kenny = User.create!(email: "kenny@gmail.com", password: "12345678")
+    steven = User.create!(email: "steven@example.com", password: "12345678")
+    swag202 = Course.create!(topic: "master the art of swag")
+    swag202.teacher = tim
+    swag202.save
+
+    swag202.students << kenny
+    swag202.students << steven
+    swag202.students << steven
+
+    assert_equal 1, steven.courses_taken.size
+
+  end
+
+  test "user can register for 2 clases" do
+    kenny = User.create!(email: "kenny@gmail.com", password: "12345678")
+    reading101 = Course.create!(topic: "learn to read code really good")
+    swag202 = Course.create!(topic: "master the art of swag")
+
+    swag202.students    << kenny
+    reading101.students << kenny
+
+    assert_equal 2, kenny.courses_taken.size
+
+  end
+
+
 end

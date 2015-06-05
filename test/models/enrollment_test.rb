54c8989936186885e7e915d1a1db5582777a618b
xichi <xichi@github.com>
2015-06-05 15:30:22 -0400

    cleans up test code

--- test/models/enrollment_test.rb
+++ test/models/enrollment_test.rb
@@ -5,7 +5,7 @@ class EnrollmentTest < ActiveSupport::TestCase
   test "user cannot take the class they are teaching" do
     tim = User.create!(email: "tmurray002@gmail.com", password: "12345678")
     swag202 = Course.create!(topic: "master the art of swag", teacher: tim)
-    
+
     assert_raise ActiveRecord::RecordInvalid do
       swag202.students << tim
     end
@@ -13,15 +13,10 @@ class EnrollmentTest < ActiveSupport::TestCase
   end
 
   test "user cannot take the same class twice" do
-    tim = User.create!(email: "tmurray002@gmail.com", password: "12345678")
-    kenny = User.create!(email: "kenny@gmail.com", password: "12345678")
     steven = User.create!(email: "steven@example.com", password: "12345678")
     swag202 = Course.create!(topic: "master the art of swag")
-    swag202.teacher = tim
-    swag202.save
-
-    swag202.students << kenny
     swag202.students << steven
+
     assert_raise ActiveRecord::RecordInvalid do
       swag202.students << steven
     end

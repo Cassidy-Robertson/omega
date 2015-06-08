f4ae17ecb40480fbb0dca66e170f4a642dafa25b
wperera6 <wperera6@gmail.com>
2015-06-08 10:34:26 -0400

    add migration date time street city state zipcode description duration and add to courses view page

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -6,6 +6,10 @@ class EnrollmentsController < ApplicationController
     @enrollment.student_id = current_user.id
     @enrollment.save
 
+
+    course = Course.find_by :id => params[:course_id]
+    course.teacher.notifications.create(text: "#{current_user.name} has just joined your class, on #{course.topic}.")
+
     redirect_to "/profiles/#{current_user.id}"
   end
 

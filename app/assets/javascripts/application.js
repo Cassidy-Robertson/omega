fbe1450169a5835cee05f590a023f709b9d2d698
wperera6 <wperera6@gmail.com>
2015-06-11 14:13:00 -0400

    BootStrapstylesheet add to app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+//= require vendor_javascript
 //= require turbolinks
 //= require_tree .
-
-require gritter
---
+++ vendor/assets/javascripts/vendor_javascript.js
@@ -0,0 +1,16 @@
+
+//= require jquery.mb.YTPlayer.min
+//= require appear
+//= require jquery.simple-text-rotator.min
+//= require jqBootstrapValidation
+//= require gmaps
+//= require isotope.pkgd.min
+//= require imagesloaded.pkgd
+//= require jquery.flexslider-min
+//= require jquery.magnific-popup.min
+//= require jquery.fitvids
+//= require smoothscroll
+//= require wow.min
+//= require owl.carousel.min
+//= require contact
+//= require custom
